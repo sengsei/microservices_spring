67a808777832f28c1bc8007fe0ae64dd7f0b2e02
sengsei <sebastian.sengbusch@stud.th-luebeck.de>
2022-08-26 17:24:18 +0200

    create rental test works

--- rental/src/main/java/de/prototype/rental/controller/RentalController.java
+++ rental/src/main/java/de/prototype/rental/controller/RentalController.java
@@ -3,26 +3,26 @@ package de.prototype.rental.controller;
 import de.prototype.rental.dto.RentalDto;
 import de.prototype.rental.service.RentalService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @AllArgsConstructor
 @RestController
+@RequestMapping("/api/rentals")
 public class RentalController {
 
     private RentalService rentalService;
 
-    @GetMapping("/api/rentals")
+    @GetMapping()
     public Flux<RentalDto> findAll() {
         return rentalService.findAll();
     }
 
-    @PostMapping("/api/rentals")
+    @PostMapping()
+    @ResponseStatus(HttpStatus.CREATED)
     public Mono<ResponseEntity<RentalDto>> save(@RequestBody Mono<RentalDto> rentalDtoMono) {
         return rentalService.save(rentalDtoMono).map(ResponseEntity::ok);
     }
