7cd2e61492e273589128d8affe6a00e979d98908
sengsei <sebastian.sengbusch@stud.th-luebeck.de>
2022-09-24 12:43:52 +0200

    route with loadbalancer works (without gateway)

--- gateway/src/main/java/de/prototype/gateway/GatewayApplication.java
+++ gateway/src/main/java/de/prototype/gateway/GatewayApplication.java
@@ -2,17 +2,22 @@ package de.prototype.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.reactive.function.client.WebClient;
+
 
 @SpringBootApplication
 public class GatewayApplication {
 
+
     @Bean
-    @LoadBalanced
-    public WebClient.Builder loadBalancedWebClientBuilder() {
-        return WebClient.builder();
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+        return builder.routes().route(
+                p -> p
+                        .path("/bla")
+                        .uri("http://app:8080/api/rentals-and-comments/**")
+        ).build();
     }
 
 
